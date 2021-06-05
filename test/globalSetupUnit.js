a7ec6d397c9e4df80609b82529ca06f9f0785bde
subramaniashiva <vengaishiva@gmail.com>
2021-06-05 19:43:01 +0100

    test: use sinon for stubs

--- test/globalSetupUnit.js
+++ test/globalSetupUnit.js
@@ -1,5 +1,13 @@
-const { expect } = require('chai');
+const chai = require('chai');
+const sinonChai = require('sinon-chai');
+const chaiAsPromised = require('chai-as-promised');
+const sinon = require('sinon');
+
+chai.use(sinonChai);
+chai.use(chaiAsPromised);
+const { expect } = chai;
 
 before(() => {
   global.expect = expect;
+  global.sinon = sinon;
 });
