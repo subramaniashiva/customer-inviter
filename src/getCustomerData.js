b67721222d19e44a716d26d010925962e82117b5
subramaniashiva <vengaishiva@gmail.com>
2021-06-07 01:34:30 +0100

    feat: parse and model customer data

--- src/getCustomerData.js
+++ src/getCustomerData.js
@@ -1,8 +1,18 @@
-module.exports = ({ readRemoteFile, logger }) => async ({ customerDataUrl }) => {
+module.exports = ({ readRemoteFile, logger, customerModel }) => async ({ customerDataUrl }) => {
   try {
-    const fileContents = await readRemoteFile({ filePathUrl: customerDataUrl });
-    logger.info({ fileContents }, 'file read successfully');
-    return fileContents;
+    const rawfileContents = await readRemoteFile({ filePathUrl: customerDataUrl });
+    const customerDataArray = rawfileContents.split('\n').reduce((modelledArray, row) => {
+      try {
+        const parsedRow = JSON.parse(row);
+        const customer = customerModel(parsedRow);
+        modelledArray.push(customer);
+      } catch (err) {
+        logger.error({ err, row }, 'Error while parsing a row in customer data');
+      }
+      return modelledArray;
+    }, []);
+    logger.info('file read successfully');
+    return customerDataArray;
   } catch (err) {
     logger.error({ err }, 'Error while getting customer data');
     throw err;
