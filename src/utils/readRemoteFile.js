e05e6471c9a8235906aad6496a91580faab848b1
subramaniashiva <vengaishiva@gmail.com>
2021-06-07 01:33:32 +0100

    feat: add info logs when reading remote file

--- src/utils/readRemoteFile.js
+++ src/utils/readRemoteFile.js
@@ -1,8 +1,9 @@
-module.exports = ({ axios, isValidUrl }) => async ({ filePathUrl } = {}) => {
+module.exports = ({ axios, isValidUrl, logger }) => async ({ filePathUrl } = {}) => {
   if (!filePathUrl || typeof filePathUrl !== 'string' || !isValidUrl({ urlString: filePathUrl })) {
     throw new TypeError('filePathUrl must be a valid url');
   }
 
+  logger.info(`reading file from ${filePathUrl}`);
   const { data } = await axios.get(filePathUrl);
   return data;
 };
